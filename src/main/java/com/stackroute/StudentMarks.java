e7a5eb1e4a960e98f0ef5fe082014c410eb503a6
gekadal1 <gekadal1@in.ibm.com>
2018-12-27 18:00:52 +0530

    javacode for 1,3

--- src/main/java/com/stackroute/StudentMarks.java
+++ src/main/java/com/stackroute/StudentMarks.java
@@ -1,11 +1,22 @@
 package com.stackroute;
 
-import java.util.List;
-
 public class StudentMarks {
-    public static boolean studentmarks(int grade) {
-        return true;
+    public static boolean studentmarks(int grade)
+
+    {
+        if(grade<0 && grade>100)
+        {
+            return true;
+        }
+        else if(grade<0)
+        {
+            return false;
+        }
+        else if(grade>100)
+        {
+            return false;
+        }
+        else
+            return true;
     }
-
-
 }
