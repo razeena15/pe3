b298adfb07af4ad6a9920d98eb022fbebd365702
debmalyap <debmalyapaulfbp@gmail.com>
2018-12-27 16:21:47 +0530

    Files added

--- src/test/java/com/stackroute/ChessBoardPatternTest.java
+++ src/test/java/com/stackroute/ChessBoardPatternTest.java
@@ -20,20 +20,20 @@ public class ChessBoardPatternTest {
     //Test case 1:- count the number of columns,it's always 8,less than or greater than 8 is an error
     public void TestCase1()
     {
-        assertEquals("Number of Columns is 8",ChessBoardPattern.CountColumns());
+        assertEquals("Number of Columns is 8",ChessBoardPattern.CountColumns(7));
     }
 
     @Test
     //Test case 2:- count the number of rows,it's always 8,less than or greater than 8 is an error
     public void TestCase2()
     {
-        assertEquals("Number of Rows is 8",ChessBoardPattern.CountRows());
+        assertEquals("Number of Rows is 8",ChessBoardPattern.CountRows(6));
     }
 
     @Test
     //Test case 3:- Check the first cell of the matrix,i.e it is always "WW".
     public void TestCase3()
     {
-        assertEquals("Number of Rows is 8",ChessBoardPattern.CheckFirstCell());
+        assertEquals("Number of Rows is 8",ChessBoardPattern.CheckFirstCell("BW"));
     }
 }
