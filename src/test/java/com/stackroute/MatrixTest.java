2421b00a11d13ef6c09e4621131e33cd4c67dba3
razeej21 <razeej21@in.ibm.com>
2018-12-27 15:22:46 +0530

    Modified matrix

--- src/test/java/com/stackroute/MatrixTest.java
+++ src/test/java/com/stackroute/MatrixTest.java
@@ -12,13 +12,13 @@ public class MatrixTest {
     @Test
     public void sumOfMatrix()
     {
-        int[][] result = m.sumOfMatrix(new int[][]{{1, 2, 3}, {4, 5, 6}}, new int[][]{{9, 8, 7}, {6, 5, 4}});
+        int[][] result = m.sumOfMatrix(new int[][]{{1, 2},{3,4},{ 5, 6}}, new int[][]{{9, 8},{ 7,6},{ 5, 4}});
         assertArrayEquals(new int[][]{{10,10},{10,10},{10,10}},result);
     }
     @Test
     public void sumOfMatrix2()
     {
-        int[][] result = m.sumOfMatrix(new int[][]{{1, 2, 3}, {4, 5, 6}}, new int[][]{{9, 8, 7}, {6, 5, 4}});
+        int[][] result = m.sumOfMatrix(new int[][]{{1, 2},{3,4},{ 5, 6}}, new int[][]{{9, 8},{ 7,6},{ 5, 4}});
         assertArrayEquals(new int[][]{{10,10},{8,10},{7,10}},result);
     }
 
