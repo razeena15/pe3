8a27c9cebac3a383c570702f8efdbe999d1ff790
sunigu11 <sunilgupta0739@gmail.com>
2018-12-28 10:03:23 +0530

    updated the test case of question no 7

--- src/main/java/com/stackroute/ExceptionPractice.java
+++ src/main/java/com/stackroute/ExceptionPractice.java
@@ -7,12 +7,12 @@ public class ExceptionPractice {
     public ExceptionPractice(String string){
         //constructor
     }
-    public boolean main(int [] arr){
+    public String main(){
 
         //write code within try and catch block
 
 
-        return true;//change the return type as it will return as by default it is fault
+        return null;
     }
 }
 
